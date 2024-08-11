e74303b72283ad6ccd1d873759316b0ca066968f
realrnvr <realrnvr@gmail.com>
2024-08-11 21:59:48 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -26,13 +26,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.options(
-  "*",
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.options("*", cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("API Spining!");
