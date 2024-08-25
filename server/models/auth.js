b806ec66b5a45426f6f6df1cdb9ccaba9ac3e2d4
realrnvr <realrnvr@gmail.com>
2024-08-25 18:53:00 +0530

    Validator

--- server/models/auth.js
+++ server/models/auth.js
@@ -15,6 +15,7 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Pls provide a valid Email",
     ],
+    // Not a validator
     unique: true,
   },
   password: {
